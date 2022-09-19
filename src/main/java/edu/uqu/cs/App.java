c09a8f6eaac2366469b90865e73e6d628efd07a7
ANASALMALAYU <s443011889@st.uqu.edu.sa>
2022-09-19 15:15:14 +0300

    my first git commit

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -11,10 +11,10 @@ package edu.uqu.cs;
  *      b.	Change <Student ID> to your student ID
  */
  
-class FirstExercise
+public class App
 {
-    private static void mainMethod( String[] args )
+    public static void main ( String[] args ) 
     {
-        System.out.println( "Hello World!" );
+    System.out.println("Hello: Anas Ismail Almalayu, 443011889");
     }
 }
